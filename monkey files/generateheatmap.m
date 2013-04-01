b88e6a381fd5d654bed52688e17fb06527b333d0
Teja <maddalihanumateja@gatech.edu>
2013-04-01 19:28:45 -0400

    Combined program and Heat map program
    The generatetrackandbtf.m program combines all the files that were
    written previously except for the visualization program. generateheatmap
    uses a 2-D histogram to create a heatmap so as to visualise how
    frequently a monkey visits a particular location in the cage. The plan
    is to generate these heatmaps and matrix of interaction for every day
    seperately

---
+++ monkey files/generateheatmap.m
@@ -0,0 +1,27 @@
+function generateheatmap(monkeyid,noofhistogrambinsx,noofhistogrambinsy,cagemin,cagemax)
+% Program to generate a heat map that shows the frequency of visiting a
+% particular region in the cage
+%noofhistogrambinsx = number of bins used in calculating the histogram for x axis
+%noofhistogrambinsy = number of bins used in calculating the histogram for y axis
+%cagemin = minimum value of cage coordinates in any direction (assuming the cage is a cube) 
+%cagemax = maximum value of cage coordinates in any direction
+%We calculate the histogram only over the range [cagemin,cagemax] 
+
+dat=evalin('base',strcat('monkey',int2str(monkeyid),'track(find(monkey',int2str(monkeyid),'track(:,1)>0),3:4);'));
+n = hist3(dat,[noofhistogrambinsx,noofhistogrambinsy])/length(dat); % Extract histogram data;
+% Divide the cage area into a noofhistogrambinsx by noofhistogrambinsy
+% matrix and calculate the histogram of monkeyid's position
+n1 = n';
+n1( size(n,1) + 1 ,size(n,2) + 1 ) = 0;
+% Generate grid for 2-D projected view of intensities
+xb = linspace(cagemin,cagemax,size(n,1)+1);
+yb = linspace(cagemin,cagemax,size(n,1)+1);
+% Make a pseudocolor plot on this grid
+h = pcolor(xb,yb,n1);
+% Set the z-level and colormap of the displayed grid
+colormap(hot) % heat map
+title('Intensity Map');
+xlabel('X-Axis');
+ylabel('Y-Axis');
+%-- 1/4/13  6:42 PM --%
+end
---
+++ monkey files/generatetrackandbtf.m
@@ -0,0 +1,26 @@
+function generatetrackandbtf(framerate,medianfilterorder,threshold)
+% This will act as the master program for generating all the monkey btf
+% files.
+% Given the data in csv files named as "monthlong - day 1.txt","monthlong - day 2.txt"
+% etc present in the working folder we provide the following additional
+% parameters - framerate required for the final videos, medianfilterorder
+% used when filtering for outliers and threshold i.e distance threshold
+% within which 2 monkeys are assumed to be interacting
+
+for i=1:47 % We have 47 files for the monkey data (Change this into an input variable for a generalised function later on)
+[m,monkeygroup2tag,tags]=seperateindividualtags(['monthlong - day ',int2str(i),'.txt'],'collar_tags_map_monthlong.txt'); 
+tstart=m(1,2);
+tstop=m(end,2);
+assignin('base','tstart',tstart); % tstart, tstop, m and monkeygroup2tag should be present in the base workspace since the generateallmonkeytracks, findmatrixofinteraction and createbtfsourcedata extarct them from there and not from the generatetrackandbtf's stack.
+assignin('base','tstop',tstop);
+assignin('base','m',m);
+assignin('base','monkeygroup2tag',monkeygroup2tag);
+generateallmonkeytracks(tstart,tstop,monkeygroup2tag,framerate,medianfilterorder);
+matrixofinteraction=findmatrixofinteraction(monkeygroup2tag,threshold);
+monkeybtf=createbtfsourcedata(monkeygroup2tag);
+save(strcat('day',int2str(i),'matrixofinteraction.mat'),'matrixofinteraction'); % Store the generated matrixofinteraction in a .mat file
+save(strcat('day',int2str(i),'btf.mat'),'monkeybtf'); % Store the generated monkeybtf in a .mat file
+disp(i);
+end
+
+end
