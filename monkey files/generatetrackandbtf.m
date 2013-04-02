d4abb6c9fc7c9357dd1633dc2c3bf2496ad2ddad
Teja <maddalihanumateja@gatech.edu>
2013-04-02 17:15:01 -0400

    included input to findmatrixofinteraction
    monkeyradius input was missing previously. Added this

--- monkey files/generatetrackandbtf.m
+++ monkey files/generatetrackandbtf.m
@@ -6,6 +6,7 @@ function generatetrackandbtf(framerate,medianfilterorder,threshold)
 % parameters - framerate required for the final videos, medianfilterorder
 % used when filtering for outliers and threshold i.e distance threshold
 % within which 2 monkeys are assumed to be interacting
+monkeyradius=0.025;
 
 for i=1:47 % We have 47 files for the monkey data (Change this into an input variable for a generalised function later on)
 [m,monkeygroup2tag,tags]=seperateindividualtags(['monthlong - day ',int2str(i),'.txt'],'collar_tags_map_monthlong.txt'); 
@@ -16,7 +17,7 @@ assignin('base','tstop',tstop);
 assignin('base','m',m);
 assignin('base','monkeygroup2tag',monkeygroup2tag);
 generateallmonkeytracks(tstart,tstop,monkeygroup2tag,framerate,medianfilterorder);
-matrixofinteraction=findmatrixofinteraction(monkeygroup2tag,threshold);
+matrixofinteraction=findmatrixofinteraction(monkeygroup2tag,threshold,monkeyradius);
 monkeybtf=createbtfsourcedata(monkeygroup2tag);
 save(strcat('day',int2str(i),'matrixofinteraction.mat'),'matrixofinteraction'); % Store the generated matrixofinteraction in a .mat file
 save(strcat('day',int2str(i),'btf.mat'),'monkeybtf'); % Store the generated monkeybtf in a .mat file
